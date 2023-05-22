47f31bc505ab579328e6debb09cac1ef485aba71
searleser97 <gabrielsanv97@gmail.com>
2023-05-22 14:35:40 -0700

    update

--- mappings.lua
+++ mappings.lua
@@ -5,10 +5,6 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
   },
-
-  v = {
-    ["<C-c>"] = {'"+y', "Copy selection to system clipboard"}
-  }
 }
 
 -- more keybinds!
