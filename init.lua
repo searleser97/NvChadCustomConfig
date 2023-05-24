45df8bfd3d32e7fa6f661c7a1efdfacf99cc8361
searleser97 <gabrielsanv97@gmail.com>
2023-05-24 13:17:18 -0700

    fix entering buffers in insert mode

--- init.lua
+++ init.lua
@@ -8,3 +8,12 @@
 local opt = vim.opt
 
 opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
+
